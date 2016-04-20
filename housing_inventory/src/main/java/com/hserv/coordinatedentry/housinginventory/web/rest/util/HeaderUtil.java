ba56a66b980d83e7838b11163c5efd914a2c25dc
shubas <shubas.scjp03@gmail.com>
2016-04-20 11:44:52 -0400

    util -- housing inventory

--- housing_inventory/src/main/java/com/hserv/coordinatedentry/housinginventory/web/rest/util/HeaderUtil.java
+++ housing_inventory/src/main/java/com/hserv/coordinatedentry/housinginventory/web/rest/util/HeaderUtil.java
@@ -10,27 +10,27 @@ public class HeaderUtil {
 
     public static HttpHeaders createAlert(String message, String param) {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("X-himsAppApp-alert", message);
-        headers.add("X-himsAppApp-params", param);
+        headers.add("X-housingInventoryApp-alert", message);
+        headers.add("X-housingInventoryApp-params", param);
         return headers;
     }
 
     public static HttpHeaders createEntityCreationAlert(String entityName, String param) {
-        return createAlert("himsAppApp." + entityName + ".created", param);
+        return createAlert("housingInventoryApp." + entityName + ".created", param);
     }
 
     public static HttpHeaders createEntityUpdateAlert(String entityName, String param) {
-        return createAlert("himsAppApp." + entityName + ".updated", param);
+        return createAlert("housingInventoryApp." + entityName + ".updated", param);
     }
 
     public static HttpHeaders createEntityDeletionAlert(String entityName, String param) {
-        return createAlert("himsAppApp." + entityName + ".deleted", param);
+        return createAlert("housingInventoryApp." + entityName + ".deleted", param);
     }
 
     public static HttpHeaders createFailureAlert(String entityName, String errorKey, String defaultMessage) {
         HttpHeaders headers = new HttpHeaders();
-        headers.add("X-himsAppApp-error", "error." + errorKey);
-        headers.add("X-himsAppApp-params", entityName);
+        headers.add("X-housingInventoryApp-error", "error." + errorKey);
+        headers.add("X-housingInventoryApp-params", entityName);
         return headers;
     }
 }
