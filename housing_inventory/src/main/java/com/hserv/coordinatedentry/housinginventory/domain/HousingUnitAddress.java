fb2edb1616d406ba04e3114899e164034d56233d
shubas <shubas.scjp03@gmail.com>
2016-05-09 11:07:16 -0400

    entities

--- housing_inventory/src/main/java/com/hserv/coordinatedentry/housinginventory/domain/HousingUnitAddress.java
+++ housing_inventory/src/main/java/com/hserv/coordinatedentry/housinginventory/domain/HousingUnitAddress.java
@@ -1,91 +1,103 @@
 package com.hserv.coordinatedentry.housinginventory.domain;
-
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
-
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.Objects;
+import java.time.LocalDateTime;
 import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-/**
- * A HousingUnitAddress.
- */
+
 @Entity
-@Table(name = "housing_unit_address")
-@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-public class HousingUnitAddress extends HousingInventoryBaseEntity implements Serializable {
+@Table(name="housing_unit_address",schema="housing_inventory")
+public class HousingUnitAddress  extends HousingInventoryBaseEntity {
+	
+	private static final long serialVersionUID = 1228941176751521786L;
 
-    private static final long serialVersionUID = 1L;
-
-    @Id
-    @Column(name = "address_id")
-    private UUID addressId;
-
-    @Column(name = "addressline_1")
-    private String addressline1;
-
-    @Column(name = "addressline_2")
-    private String addressline2;
-
-    @Column(name = "city")
-    private String city;
-
-    @Column(name = "state")
-    private String state;
-
-    @Column(name = "zip")
-    private String zip;
-
-    @ManyToOne
-    private HousingInventory housingInventory;
+	@Id
+	@Column(name="address_id")
+	private UUID addressId;
+	
+	@Column(name="addressline_1")
+	private String line1;
+	
+	@Column(name="addressline_2")
+	private String line2;
+	
+	@Column(name="address_state")
+	private String state;
+	
+	@Column(name="address_city")
+	private String city;
+	
+	@Column(name="zip_code")
+	private Integer zipCode;
+	
+	@ManyToOne
+	@JsonIgnore
+	private HousingInventory housingInventory;
+	
+	@Transient
+	private String housingInventoryId;
+	
+    public HousingUnitAddress(){
+		
+	}
+	
+	public HousingUnitAddress(UUID addressId, String line1, String line2,
+			String state, String city, Integer zipCode,HousingInventory housingInventory,  LocalDateTime dateCreated,
+			LocalDateTime dateUpdated, Boolean inactive) {
+		super(dateCreated, dateUpdated, inactive);
+		this.addressId = addressId;
+		this.line1 = line1;
+		this.line2 = line2;
+		this.state = state;
+		this.city = city;
+		this.zipCode = zipCode;
+		this.housingInventory=housingInventory;
+	}
 
 	public UUID getAddressId() {
 		return addressId;
 	}
 
-	public void setAddressId(UUID addressId) {
+    public void setAddressId(UUID addressId) {
 		this.addressId = addressId;
 	}
 
-	public String getAddressline1() {
-		return addressline1;
+	public Integer getZipCode() {
+		return zipCode;
 	}
 
-	public void setAddressline1(String addressline1) {
-		this.addressline1 = addressline1;
+	public void setZipCode(Integer zipCode) {
+		this.zipCode = zipCode;
 	}
 
-	public String getAddressline2() {
-		return addressline2;
+	public String getLine1() {
+		return line1;
 	}
-
-	public void setAddressline2(String addressline2) {
-		this.addressline2 = addressline2;
+	public void setLine1(String line1) {
+		this.line1 = line1;
 	}
-
-	public String getCity() {
-		return city;
+	public String getLine2() {
+		return line2;
 	}
-
-	public void setCity(String city) {
-		this.city = city;
+	public void setLine2(String line2) {
+		this.line2 = line2;
 	}
-
 	public String getState() {
 		return state;
 	}
-
 	public void setState(String state) {
 		this.state = state;
 	}
-
-	public String getZip() {
-		return zip;
+	public String getCity() {
+		return city;
 	}
-
-	public void setZip(String zip) {
-		this.zip = zip;
+	public void setCity(String city) {
+		this.city = city;
 	}
 
 	public HousingInventory getHousingInventory() {
@@ -95,10 +107,81 @@ public class HousingUnitAddress extends HousingInventoryBaseEntity implements Se
 	public void setHousingInventory(HousingInventory housingInventory) {
 		this.housingInventory = housingInventory;
 	}
-    
-    
 
-      
+	public String getHousingInventoryId() {
+		return housingInventoryId;
+	}
 
-   
+	public void setHousingInventoryId(String housingInventoryId) {
+		this.housingInventoryId = housingInventoryId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((addressId == null) ? 0 : addressId.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((housingInventory == null) ? 0 : housingInventory.hashCode());
+		result = prime * result + ((housingInventoryId == null) ? 0 : housingInventoryId.hashCode());
+		result = prime * result + ((line1 == null) ? 0 : line1.hashCode());
+		result = prime * result + ((line2 == null) ? 0 : line2.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HousingUnitAddress other = (HousingUnitAddress) obj;
+		if (addressId == null) {
+			if (other.addressId != null)
+				return false;
+		} else if (!addressId.equals(other.addressId))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (housingInventory == null) {
+			if (other.housingInventory != null)
+				return false;
+		} else if (!housingInventory.equals(other.housingInventory))
+			return false;
+		if (housingInventoryId == null) {
+			if (other.housingInventoryId != null)
+				return false;
+		} else if (!housingInventoryId.equals(other.housingInventoryId))
+			return false;
+		if (line1 == null) {
+			if (other.line1 != null)
+				return false;
+		} else if (!line1.equals(other.line1))
+			return false;
+		if (line2 == null) {
+			if (other.line2 != null)
+				return false;
+		} else if (!line2.equals(other.line2))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (zipCode == null) {
+			if (other.zipCode != null)
+				return false;
+		} else if (!zipCode.equals(other.zipCode))
+			return false;
+		return true;
+	}
+	
+	
 }
--- housing_inventory/src/main/java/com/hserv/coordinatedentry/housinginventory/domain/HousingUnitAssignment.java
+++ housing_inventory/src/main/java/com/hserv/coordinatedentry/housinginventory/domain/HousingUnitAssignment.java
@@ -1,26 +1,29 @@
 package com.hserv.coordinatedentry.housinginventory.domain;
-
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
-
-import javax.persistence.*;
 import java.io.Serializable;
-import java.time.ZonedDateTime;
-import java.util.Objects;
+import java.time.LocalDateTime;
 import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  * A HousingUnitAssignment.
  */
 @Entity
-@Table(name = "housing_unit_assignment")
-@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-public class HousingUnitAssignment extends HousingInventoryBaseEntity implements Serializable {
+@Table(name = "housing_unit_assignment", schema="housing_inventory")
+public class HousingUnitAssignment extends HousingInventoryBaseEntity  {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -2423704859507175452L;
 
-   
-    @Id
+	@Id
     @Column(name = "assignment_id")
     private UUID assignmentId;
 
@@ -31,50 +34,120 @@ public class HousingUnitAssignment extends HousingInventoryBaseEntity implements
     private String householdId;
 
     @Column(name = "checkout_date")
-    private ZonedDateTime checkoutDate;
+    @JsonFormat(pattern="MM-dd-yyyy")
+    private LocalDateTime checkoutDate;
 
-    @ManyToOne
+    @ManyToOne(cascade=CascadeType.PERSIST)
+    @JsonIgnore
     private HousingInventory housingInventory;
+    
+    @Transient
+    private String housingInventoryId;
 
-	public UUID getAssignmentId() {
-		return assignmentId;
-	}
-
-	public void setAssignmentId(UUID assignmentId) {
-		this.assignmentId = assignmentId;
-	}
-
-	public String getClientId() {
-		return clientId;
-	}
-
-	public void setClientId(String clientId) {
-		this.clientId = clientId;
-	}
-
-	public String getHouseholdId() {
-		return householdId;
-	}
-
-	public void setHouseholdId(String householdId) {
-		this.householdId = householdId;
-	}
-
-	public ZonedDateTime getCheckoutDate() {
-		return checkoutDate;
-	}
-
-	public void setCheckoutDate(ZonedDateTime checkoutDate) {
-		this.checkoutDate = checkoutDate;
-	}
-
-	public HousingInventory getHousingInventory() {
-		return housingInventory;
-	}
-
-	public void setHousingInventory(HousingInventory housingInventory) {
-		this.housingInventory = housingInventory;
-	}
-
-	
+    public UUID getAssignmentId() {
+        return assignmentId;
     }
+
+    public void setAssignmentId(UUID assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getHouseholdId() {
+        return householdId;
+    }
+
+    public void setHouseholdId(String householdId) {
+        this.householdId = householdId;
+    }
+
+    public LocalDateTime getCheckoutDate() {
+        return checkoutDate;
+    }
+
+    public void setCheckoutDate(LocalDateTime checkoutDate) {
+        this.checkoutDate = checkoutDate;
+    }
+
+    public HousingInventory getHousingInventory() {
+        return housingInventory;
+    }
+
+    public void setHousingInventory(HousingInventory housingInventory) {
+        this.housingInventory = housingInventory;
+    }
+
+	public String getHousingInventoryId() {
+		return housingInventoryId;
+	}
+
+	public void setHousingInventoryId(String housingInventoryId) {
+		this.housingInventoryId = housingInventoryId;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((assignmentId == null) ? 0 : assignmentId.hashCode());
+		result = prime * result + ((checkoutDate == null) ? 0 : checkoutDate.hashCode());
+		result = prime * result + ((clientId == null) ? 0 : clientId.hashCode());
+		result = prime * result + ((householdId == null) ? 0 : householdId.hashCode());
+		result = prime * result + ((housingInventory == null) ? 0 : housingInventory.hashCode());
+		result = prime * result + ((housingInventoryId == null) ? 0 : housingInventoryId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HousingUnitAssignment other = (HousingUnitAssignment) obj;
+		if (assignmentId == null) {
+			if (other.assignmentId != null)
+				return false;
+		} else if (!assignmentId.equals(other.assignmentId))
+			return false;
+		if (checkoutDate == null) {
+			if (other.checkoutDate != null)
+				return false;
+		} else if (!checkoutDate.equals(other.checkoutDate))
+			return false;
+		if (clientId == null) {
+			if (other.clientId != null)
+				return false;
+		} else if (!clientId.equals(other.clientId))
+			return false;
+		if (householdId == null) {
+			if (other.householdId != null)
+				return false;
+		} else if (!householdId.equals(other.householdId))
+			return false;
+		if (housingInventory == null) {
+			if (other.housingInventory != null)
+				return false;
+		} else if (!housingInventory.equals(other.housingInventory))
+			return false;
+		if (housingInventoryId == null) {
+			if (other.housingInventoryId != null)
+				return false;
+		} else if (!housingInventoryId.equals(other.housingInventoryId))
+			return false;
+		return true;
+	}
+    
+    
+
+}
+
