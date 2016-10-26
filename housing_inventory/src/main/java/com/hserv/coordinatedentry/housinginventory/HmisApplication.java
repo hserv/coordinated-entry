dd8c6f4a08af02fc02f5cb4d1239bc21a098441d
suryayadavalli <surya.yadavalli@gmail.com>
2016-10-26 11:59:05 +0530

    (inventory-api)Datasource changes

--- housing_inventory/src/main/java/com/hserv/coordinatedentry/housinginventory/HmisApplication.java
+++ housing_inventory/src/main/java/com/hserv/coordinatedentry/housinginventory/HmisApplication.java
@@ -1,18 +1,20 @@
 package com.hserv.coordinatedentry.housinginventory;
 
 import javax.inject.Inject;
+import javax.sql.DataSource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.hateoas.config.EnableEntityLinks;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.servinglynk.hmis.warehouse.client.config.SpringConfig;
@@ -40,4 +42,18 @@ public class HmisApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(HmisApplication.class, args);
 	}
+	
+	@Bean
+	public DataSource dataSource() {
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		
+		dataSource.setDriverClassName(env.getRequiredProperty("spring.datasource.driver-class-name"));
+		dataSource.setUrl(env.getRequiredProperty("spring.datasource.url"));
+		dataSource.setUsername(env.getRequiredProperty("spring.datasource.username"));
+		dataSource.setPassword(env.getRequiredProperty("spring.datasource.password"));
+		
+		return dataSource;
+	}
+
 }
