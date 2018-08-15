238869b104bdc7f9eb480caa72a7b1dc154c4e78
suryayadavalli <surya.yadavalli@gmail.com>
2018-08-15 13:16:58 +0530

    Fixes in questions and picklist values responses

--- hmis-survey-api/src/main/java/com/servinglynk/hmis/warehouse/service/PickListValueService.java
+++ hmis-survey-api/src/main/java/com/servinglynk/hmis/warehouse/service/PickListValueService.java
@@ -4,11 +4,12 @@ import java.util.UUID;
 
 import com.servinglynk.hmis.warehouse.core.model.PickListValue;
 import com.servinglynk.hmis.warehouse.core.model.PickListValues;
+import com.servinglynk.hmis.warehouse.core.model.PickListValues2;
 public interface PickListValueService {
 
    PickListValues createPickListValue(UUID pickListGroupId, PickListValues pickListValues,String caller);
    PickListValue updatePickListValue(PickListValue PickListValue,String caller);
    PickListValue deletePickListValue(UUID PickListValueId,String caller);
    PickListValue getPickListValueById(UUID PickListValueId);
-   PickListValues getAllPickListGroupPickListValues(UUID pickListGroupId, Integer startIndex, Integer maxItems);
+   PickListValues2 getAllPickListGroupPickListValues(UUID pickListGroupId, Integer startIndex, Integer maxItems);
 }
