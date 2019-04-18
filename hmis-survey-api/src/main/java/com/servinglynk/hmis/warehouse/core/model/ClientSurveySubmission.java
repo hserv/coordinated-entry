2ec185d44fe051b2894c31b80062ef534dadf416
suryayadavalli <surya.yadavalli@gmail.com>
2019-04-18 16:39:55 +0530

    Fix for https://github.com/hmis-api/survey-service-api/issues/6

--- hmis-survey-api/src/main/java/com/servinglynk/hmis/warehouse/core/model/ClientSurveySubmission.java
+++ hmis-survey-api/src/main/java/com/servinglynk/hmis/warehouse/core/model/ClientSurveySubmission.java
@@ -17,6 +17,7 @@ public class ClientSurveySubmission extends ClientModel {
 
    	private Client client;
 
+	private String clientLink;
    	
 	public UUID getId() {
 		return id;
@@ -54,5 +55,11 @@ public class ClientSurveySubmission extends ClientModel {
 	}
 	public void setClient(Client client) {
 		this.client = client;
+	}
+	public String getClientLink() {
+		return clientLink;
+	}
+	public void setClientLink(String clientLink) {
+		this.clientLink = clientLink;
 	}  	
 }
